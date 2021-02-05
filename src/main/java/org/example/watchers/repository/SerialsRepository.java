44ba5863ce2d8126d1c4aee0006354ae55ed8040
mplutarc <anda.ere@yandex.ru>
2021-02-05 22:46:53 +0300

    added serials class

---
+++ src/main/java/org/example/watchers/repository/SerialsRepository.java
@@ -0,0 +1,13 @@
+package org.example.watchers.repository;
+
+import org.example.watchers.entity.Serials;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface SerialsRepository extends JpaRepository<Serials, Integer> {
+
+    List<Serials> findAllByOrderByNameAsc();
+}
