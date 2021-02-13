b308e0a713b543fa92c98e70b055690cc547f984
mplutarc <anda.ere@yandex.ru>
2021-02-13 13:33:47 +0300

    working theme changer with localStorage theme is loaded during page load

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -1,3 +1,37 @@
+function setTheme(){
+    let currTheme = localStorage.getItem('theme');
+    console.log('set theme =' + currTheme);
+    link.setAttribute("href", currTheme);
+}
+
+let btn = document.getElementById("theme-button");
+let link = document.getElementById("theme-link");
+
+btn.addEventListener("click", function () { ChangeTheme(); });
+
+function ChangeTheme()
+{
+    let lightTheme = "/css/light.css";
+    let darkTheme = "/css/dark.css";
+
+    let currTheme = link.getAttribute("href");
+    console.log('currTheme ' + currTheme);
+    let theme = "";
+
+    if(currTheme === lightTheme)
+    {
+        currTheme = darkTheme;
+        theme = "dark";
+    }
+    else
+    {
+        currTheme = lightTheme;
+        theme = "light";
+    }
+    localStorage.setItem('theme', currTheme);
+    link.setAttribute("href", currTheme);
+}
+
 function OnFilmChange() {
     let valGenre = document.getElementById("genreFilmChange").value;
     let valYear = document.getElementById("yearFilmChange").value;
@@ -13,34 +47,4 @@ function OnSerialChange() {
     newHref.searchParams.set("genre", valGenre);
     newHref.searchParams.set("hui", valYear);
     window.location.href = newHref;
-}
-
-let btn = document.getElementById("theme-button");
-let link = document.getElementById("theme-link");
-
-btn.addEventListener("click", function () { ChangeTheme(); });
-
-function ChangeTheme()
-{
-    let lightTheme = "/css/light.css";
-    let darkTheme = "/css/dark.css";
-
-    let currTheme = link.getAttribute("href");
-    console.log(currTheme);
-    let theme = "";
-
-    if(currTheme === lightTheme)
-    {
-        currTheme = darkTheme;
-        theme = "dark";
-    }
-    else
-    {
-        currTheme = lightTheme;
-        theme = "light";
-    }
-
-    link.setAttribute("href", currTheme);
-
-    // Save(theme);
 }
