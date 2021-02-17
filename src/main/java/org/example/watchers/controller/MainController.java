01c6ae583386fde29e66815d9b275a23853be5d2
mplutarc <anda.ere@yandex.ru>
2021-02-17 04:42:52 +0300

    final version!
    sorting tables
    modified menu
    preservation of select value

--- src/main/java/org/example/watchers/controller/MainController.java
+++ src/main/java/org/example/watchers/controller/MainController.java
@@ -14,91 +14,97 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Objects;
 import java.util.Random;
+import java.util.SortedSet;
+import java.util.TreeSet;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 @Controller
 public class MainController {
 
-    @Autowired
-    private FilmsRepository filmsRepository;
+	@Autowired
+	private FilmsRepository filmsRepository;
 
-    @Autowired
-    private SerialsRepository serialsRepository;
+	@Autowired
+	private SerialsRepository serialsRepository;
 
-    @GetMapping("/")
-    public String mainPage(Model model){
-        return "main";
-    }
+	@GetMapping("/")
+	public String mainPage(Model model){
+		return "main";
+	}
 
-    @RequestMapping("/choiceFilm")
-    public String getFilm(Model model,
-                          @RequestParam(value = "genre", required = false, defaultValue = "") String genre,
-                          @RequestParam(value = "year", required = false, defaultValue = "") String year)
-    {
-        val films = filmsRepository.findAll();
-        val genres = films
-                .stream()
-                .map(Films::getGenres)
-                .filter(Objects::nonNull)
-                .flatMap(it -> Stream.of(it.split("; ")))
-                .collect(Collectors.toSet());
-        model.addAttribute("genres", genres);
-        val years = films
-                .stream()
-                .map(Films::getYear)
-                .filter(Objects::nonNull)
-                .collect(Collectors.toSet());
-        model.addAttribute("years", years);
-        model.addAttribute("films", films
-                .stream()
-                .filter(it -> it.getGenres().contains(genre))
-                .filter(it -> it.getYear().contains(year))
-                .collect(Collectors.toList()));
-        return "choiceFilm";
-    }
-    @RequestMapping("/choiceSerial")
-    public String getSerial(@RequestParam(value = "genres", required = false, defaultValue = "") String genre,
-                            @RequestParam(value = "years", required = false, defaultValue = "") String year,
-                            Model model) {
-        val serials = serialsRepository.findAll();
-        val genres = serials
-                .stream()
-                .map(Serials::getGenres)
-                .filter(Objects::nonNull)
-                .flatMap(it -> Stream.of(it.split("; ")))
-                .collect(Collectors.toSet());
-        model.addAttribute("genres", genres);
-        val years = serials
-                .stream()
-                .map(Serials::getYear)
-                .filter(Objects::nonNull)
-                .collect(Collectors.toSet());
-        model.addAttribute("years", years);
-        model.addAttribute("serials", serials
-                .stream()
-                .filter(it -> it.getGenres().contains(genre))
-                .filter(it -> it.getYear().contains(year))
-                .collect(Collectors.toList()));
-        return "choiceSerial";
-    }
+	@RequestMapping("/choiceFilm")
+	public String getFilm(Model model,
+	                      @RequestParam(value = "genre", required = false, defaultValue = "") String genre,
+	                      @RequestParam(value = "year", required = false, defaultValue = "") String year)
+	{
+		val films = filmsRepository.findAll();
+		val genresSet = films
+				.stream()
+				.map(Films::getGenres)
+				.filter(Objects::nonNull)
+				.flatMap(it -> Stream.of(it.split("; ")))
+				.collect(Collectors.toSet());
+		SortedSet<String> genres = new TreeSet<>(genresSet);
+		model.addAttribute("genres", genres);
+		val yearsSet = films
+				.stream()
+				.map(Films::getYear)
+				.filter(Objects::nonNull)
+				.collect(Collectors.toSet());
+		SortedSet<String> years = new TreeSet<>(yearsSet);
+		model.addAttribute("years", years);
+		model.addAttribute("films", films
+				.stream()
+				.filter(it -> it.getGenres().contains(genre))
+				.filter(it -> it.getYear().contains(year))
+				.collect(Collectors.toList()));
+		return "choiceFilm";
+	}
+	@RequestMapping("/choiceSerial")
+	public String getSerial(@RequestParam(value = "genres", required = false, defaultValue = "") String genre,
+	                        @RequestParam(value = "years", required = false, defaultValue = "") String year,
+	                        Model model) {
+		val serials = serialsRepository.findAll();
+		val genresSet = serials
+				.stream()
+				.map(Serials::getGenres)
+				.filter(Objects::nonNull)
+				.flatMap(it -> Stream.of(it.split("; ")))
+				.collect(Collectors.toSet());
+		SortedSet<String> genres = new TreeSet<>(genresSet);
+		model.addAttribute("genres", genres);
+		val yearsSet = serials
+				.stream()
+				.map(Serials::getYear)
+				.filter(Objects::nonNull)
+				.collect(Collectors.toSet());
+		SortedSet<String> years = new TreeSet<>(yearsSet);
+		model.addAttribute("years", years);
+		model.addAttribute("serials", serials
+				.stream()
+				.filter(it -> it.getGenres().contains(genre))
+				.filter(it -> it.getYear().contains(year))
+				.collect(Collectors.toList()));
+		return "choiceSerial";
+	}
 
-    @RequestMapping("/randomFilm")
-    public String randomFilm(Model model){
-        int rand = new Random().nextInt(filmsRepository.findAll().size());
-        val randFilms = filmsRepository.findAll();
-        model.addAttribute("randFilms", randFilms
-                .stream().filter(it -> it.getNumber().equals(rand+1))
-                .collect(Collectors.toList()));
-        return "randomFilm";
-    }
-    @RequestMapping("/randomSerial")
-    public String randomSerial(Model model){
-        int rand = new Random().nextInt(serialsRepository.findAll().size());
-        val randSerials = serialsRepository.findAll();
-        model.addAttribute("randSerials", randSerials
-                .stream().filter(it -> it.getNumber().equals(rand+1))
-                .collect(Collectors.toList()));
-        return "randomSerial";
-    }
+	@RequestMapping("/randomFilm")
+	public String randomFilm(Model model){
+		int rand = new Random().nextInt(filmsRepository.findAll().size());
+		val randFilms = filmsRepository.findAll();
+		model.addAttribute("randFilms", randFilms
+				.stream().filter(it -> it.getNumber().equals(rand+1))
+				.collect(Collectors.toList()));
+		return "randomFilm";
+	}
+	@RequestMapping("/randomSerial")
+	public String randomSerial(Model model){
+		int rand = new Random().nextInt(serialsRepository.findAll().size());
+		val randSerials = serialsRepository.findAll();
+		model.addAttribute("randSerials", randSerials
+				.stream().filter(it -> it.getNumber().equals(rand+1))
+				.collect(Collectors.toList()));
+		return "randomSerial";
+	}
 }
--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -1,7 +1,29 @@
 function setTheme(){
-    let currTheme = localStorage.getItem('theme');
-    console.log('set theme =' + currTheme);
-    link.setAttribute("href", currTheme);
+	const userPrefersDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+	const userPrefersLight = window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches;
+
+
+	let currTheme;
+	if(localStorage.getItem('theme'))
+		currTheme = localStorage.getItem('theme');
+	else if(userPrefersLight)
+		currTheme = "/css/dark.css";
+	else if(userPrefersDark)
+		currTheme = "/css/light.css";
+	console.log('set theme =' + currTheme);
+	link.setAttribute("href", currTheme);
+	if(localStorage.getItem('filmGenre') != null){
+		document.getElementById("genreFilmChange").value = localStorage.getItem('filmGenre');
+	}
+	if(localStorage.getItem('filmYear') != null){
+		document.getElementById("yearFilmChange").value = localStorage.getItem('filmYear');
+	}
+	if(localStorage.getItem('serialGenre') != null){
+		document.getElementById("genreSerialChange").value = localStorage.getItem('serialGenre');
+	}
+	if(localStorage.getItem('serialYear') != null){
+		document.getElementById("yearSerialChange").value = localStorage.getItem('serialYear');
+	}
 }
 
 let btn = document.getElementById("theme-button");
@@ -11,40 +33,92 @@ btn.addEventListener("click", function () { ChangeTheme(); });
 
 function ChangeTheme()
 {
-    let lightTheme = "/css/light.css";
-    let darkTheme = "/css/dark.css";
+	let lightTheme = "/css/light.css";
+	let darkTheme = "/css/dark.css";
 
-    let currTheme = link.getAttribute("href");
-    console.log('currTheme ' + currTheme);
-    let theme = "";
-
-    if(currTheme === lightTheme)
-    {
-        currTheme = darkTheme;
-        theme = "dark";
-    }
-    else
-    {
-        currTheme = lightTheme;
-        theme = "light";
-    }
-    localStorage.setItem('theme', currTheme);
-    link.setAttribute("href", currTheme);
+	let currTheme = link.getAttribute("href");
+	console.log('currTheme ' + currTheme);
+	if(currTheme === lightTheme)
+	{
+		currTheme = darkTheme;
+	}
+	else
+	{
+		currTheme = lightTheme;
+	}
+	localStorage.setItem('theme', currTheme);
+	link.setAttribute("href", currTheme);
+	return(localStorage.getItem('theme'));
 }
+
 
 function OnFilmChange() {
-    let valGenre = document.getElementById("genreFilmChange").value;
-    let valYear = document.getElementById("yearFilmChange").value;
-    let newHref = new URL('http://localhost:8080/choiceFilm');
-    newHref.searchParams.set("genre", valGenre);
-    newHref.searchParams.set("year", valYear);
-    window.location.href = newHref;
+	let valGenre = document.getElementById("genreFilmChange").value;
+	let valYear = document.getElementById("yearFilmChange").value;
+	let newHref = new URL('http://localhost:8080/choiceFilm');
+	newHref.searchParams.set("genre", valGenre);
+	newHref.searchParams.set("year", valYear);
+	localStorage.setItem('filmGenre', valGenre);
+	localStorage.setItem('filmYear', valYear);
+
+	window.location.href = newHref;
 }
 function OnSerialChange() {
-    let valGenre = document.getElementById("genreSerialChange").value;
-    let valYear = document.getElementById("yearSerialChange").value;
-    let newHref = new URL('http://localhost:8080/choiceSerial');
-    newHref.searchParams.set("genre", valGenre);
-    newHref.searchParams.set("hui", valYear);
-    window.location.href = newHref;
+	let valGenre = document.getElementById("genreSerialChange").value;
+	let valYear = document.getElementById("yearSerialChange").value;
+	let newHref = new URL('http://localhost:8080/choiceSerial');
+	newHref.searchParams.set("genre", valGenre);
+	newHref.searchParams.set("hui", valYear);
+	localStorage.setItem('serialGenre', valGenre);
+	localStorage.setItem('serialYear', valYear);
+
+	window.location.href = newHref;
+}
+
+function clearFilm(){
+	localStorage.removeItem('filmGenre');
+	localStorage.removeItem('filmYear');
+	window.location.href = 'http://localhost:8080/choiceFilm';
+}
+function clearSerial(){
+	localStorage.removeItem('serialGenre');
+	localStorage.removeItem('serialYear');
+	window.location.href = 'http://localhost:8080/choiceSerial';
+}
+
+function sortTable(n) {
+	let table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+	table = document.getElementById("sortTable");
+	switching = true;
+	dir = "asc";
+	while (switching) {
+		switching = false;
+		rows = table.getElementsByTagName("TR");
+		for (i = 1; i < (rows.length - 1); i++) {
+			shouldSwitch = false;
+			x = rows[i].getElementsByTagName("TD")[n];
+			y = rows[i + 1].getElementsByTagName("TD")[n];
+			if (dir === "asc") {
+				if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+					shouldSwitch = true;
+					break;
+				}
+			} else if (dir === "desc") {
+				if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+					shouldSwitch = true;
+					break;
+				}
+			}
+		}
+		if (shouldSwitch) {
+			rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+			switching = true;
+			switchcount++;
+		} else {
+			if (switchcount === 0 && dir === "asc") {
+				dir = "desc";
+				switching = true;
+			}
+		}
+	}
 }
